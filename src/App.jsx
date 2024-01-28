bd688491aed13f50ca767eb25d6cbae68d999c81
Ashish bundela <ashishbundela123@gmail.com>
2024-01-28 19:42:12 +0530

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
 
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/about'
+import Projects from './pages/Projects'
+import Dashbaord from './pages/Dashbaord'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 export default function App() {
   return (
-    <div>
-      <h1 className='text-3xl text-red-500'>jhigufdytrs</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/projects' element={<Projects />}></Route>
+        <Route path='/dashbaord' element={<Dashbaord/>}></Route>
+        <Route path='/sign-in' element={<SignIn/>}></Route>
+        <Route path='/sign-up' element={<SignUp/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/Dashbaord.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dashbaord() {
+  return (
+    <div>
+      <h1>dashbaord</h1>
+    </div>
+  )
+}
